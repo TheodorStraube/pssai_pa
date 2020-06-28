bdbfa024cb26a4ea6c1152e09cae4ee649961460
Lukas Müller <lukas.mueller4@mailbox.tu-dresden.de>
2020-06-28 12:38:47 +0200

    improved hover text in gantt chart

--- visual.py
+++ visual.py
@@ -12,10 +12,13 @@ def schedule_to_gantt(schedule, jobs):
                 'Task': 'Machine {}'.format(n),
                 'Resource': jobs_names[job],
                 'Start': start,
-                'Finish': start + operation.time
+                'Finish': start + operation.time,
+                'Description': '{}, Operation {}. {} - {}'.format(jobs_names[job], job.operations.index(operation), start, start + operation.time)
              })
 
     colors = {jobs_names[job]: (random.random(), random.random(), random.random()) for job in jobs}
     fig = ff.create_gantt(data, group_tasks=True, show_colorbar=True, colors=colors, index_col='Resource')
     fig['layout']['xaxis'].update({'type': None})
+    for d in fig['data']:
+        d.update(hoverinfo='text')
     fig.show()
